01821eefdbb3975ecc8f0b59f38c91ec9d20da47
Tanisha Sharma <u8092948@anu.edu.au>
2025-07-01 01:15:22 +1000

    Day-15

--- Day_15_Coffee_Machine_Project/main.py
+++ Day_15_Coffee_Machine_Project/main.py
@@ -1,3 +1,5 @@
+from pickle import FALSE
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -30,5 +32,55 @@ resources = {
     "coffee": 100,
 }
 
-#TODO: Ask user for what they would like
+app_status = True
+money = 0.0
+
+def resources_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+def process_coins():
+    quarters = int(input("Quarters: "))
+    dimes = int(input("Dimes: "))
+    nickels = int(input("Nickels: "))
+    pennies = int(input("Pennies: "))
+    total = 0.25*quarters + 0.1*dimes + 0.05*nickels + 0.01*pennies
+    return total
+
+def payment_sufficient(payment, drink_cost):
+    if payment >= drink_cost:
+        change = round(payment - drink_cost, 2)
+        print(f"Here's your coffee cutie :))) and your change {change}")
+        global money
+        money += drink_cost
+        return True
+    else:
+        print(f"Sorry, that's not enough. Money refunded")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
+
+
+while app_status:
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if choice=="off":
+        app_status = False
+    elif choice=="report":
+        print(f"Water: {resources['water']} \nMilk: {resources['milk']} "
+              f"\nCoffee: {resources['coffee']} \nMoney: ${money}")
+    else:
+        drink = MENU[choice]
+        if resources_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if payment_sufficient(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
+
+
 
